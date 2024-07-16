fdd6420730254c5f8418ae65852ff878b85997af
jmartinez419 <justynmartinez419@gmail.com>
2024-07-16 11:04:28 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        novel new_novel = new novel();
+
+        new_novel.setTitle("A tale of two cities.");
+        System.out.println("The title is: " + new_novel.getTitle());
+    }
+}
